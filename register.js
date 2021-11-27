cfe24d42f3f239d16f49bf3cb5fd3ded4cde5733
Nahuel Flores <nahuelhernanflores25@gmail.com>
2021-11-27 16:31:29 -0300

    Modificaciones menores en style, se agrega la instalacion de sw

--- register.js
+++ register.js
@@ -1,5 +1,6 @@
 //if("serviceWorker" in navigator) Forma extensa - Aunque se usa mas en React
 if(navigator.serviceWorker) //Otra forma de hacer lo mismo, mas sencillo y entendible
+//if pregunta: ¿Service Worker es compatible con el navegador del usuario?
 {
     navigator.serviceWorker.register("./serviceWorker.js") //Permite registrar un Service Worker para poder trabajar tambien llamado sw.js    
     //En el contiene toda la informacion que se debera almacenar en cache, modificar, evaluar, comparar, eliminar, reemplazar, etc
--- serviceWorker.js
+++ serviceWorker.js
@@ -12,4 +12,23 @@ const CACHE_ELEMENTS = //ELEMENTOS QUE NOSOTROS VAMOS A CONSIDERAR PARA CACHEAR
     "./components/contador.js", //Metodo Contador
 ]
 
-const CACHE_NAME = "version_uno_cache" //Es el nombre que aparece en Cache Storage de Application en "Ver elementos"
+const CACHE_NAME = "version_uno_cache" //Es el nombre que aparece en Cache Storage de Application en "Inspeccionar elementos"
+
+//INSTALACION DE SERVICE WORKER (sw)
+self.addEventListener("install", (evento) => //Agrega/Instala un detector de Eventos
+//("Tipo de evento", funcion(nombreFuncion) =>)
+{
+    evento.waitUntil ( //Se usa para decirle al navegador que no cancele al serviceWorker(sw)
+                       //hasta que la promesa pasada waitUntil se resuelva o rechace.
+
+        caches.open(CACHE_NAME).then(cache => //ASIGNA EL NOMBRE AL SERVIDOR CACHE DEL NAVEGADOR
+        {
+            cache.addAll(CACHE_ELEMENTS).then( () => //AGREGA TODOS LOS COMPONENTS AL CACHE DEL NAV
+            {
+                self.skipWaiting();
+            });
+
+        }).catch(error => console.log(error)) //EN CASO DE ERROR, IMPRIME EN CONSOLA
+    );
+});
+
